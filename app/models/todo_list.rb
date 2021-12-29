1b429ac7693d683ff2136dc288cad86206ff2e0f
Nair Designs` <nairbdg@gmail.com>
2021-12-29 17:19:20 +0900

    initial commit

---
+++ app/models/todo_list.rb
@@ -0,0 +1,7 @@
+class TodoList < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+  accepts_nested_attributes_for :tasks, allow_destroy: true, reject_if: proc {|att| att['name'].blank?}
+  
+
+    
+end
